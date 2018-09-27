71d7b0977cff171fac2b0e2b9df261b64079f458
Lindsay Stewart <lj.stewart00@gmail.com>
2018-09-27 19:31:34 +0000

    print index of Waldo using forEach loop

---
+++ for-each-waldo.js
@@ -0,0 +1,14 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+    arr.forEach(function(arr, index) {
+      if (arr === "Waldo") {
+        found(index);   // execute callback
+      }
+    });
+  }
+  
+  function actionWhenFound(index) {
+    console.log("Found Waldo at index " + index);
+  }
+  
+  findWaldo(["Alice", "Bob","Waldo", "Winston"], actionWhenFound);
